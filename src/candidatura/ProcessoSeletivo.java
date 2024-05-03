eb97290caa30b11dcb45b66fe4c4b71feaf08234
Gileno <gileno@tsmx.net.br>
2024-05-03 17:24:22 -0300

    feat: adicionado metodo para verificacao de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,5 @@
 package candidatura;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         analisarCandidato(1900);
@@ -7,10 +7,28 @@ public class ProcessoSeletivo {
         analisarCandidato(5000);
 
         String [] candidatos ={"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        int candidatosSelecionados =0;
+        int candidatoAtual=0;
+        double salarioBase=2000.0;
 
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato=candidatos[candidatoAtual];
+            double salarioPretendido = valorPrtendido();
+
+            System.out.println("O candidato " +  candidato + " Solicitou este valor de salario " + salarioPretendido );
+
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga" );
+                candidatosSelecionados++;
+
+            }
+            candidatoAtual++;
+        }
     }
 
-   static void analisarCandidato(double salarioPretendido){
+
+
+    static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
 
         if (salarioBase > salarioPretendido){
@@ -25,4 +43,9 @@ public class ProcessoSeletivo {
             System.out.println("aguardando resultado dos demais candidatos ");
         }
     }
+
+    static Double valorPrtendido(){
+        return ThreadLocalRandom.current().nextDouble(100, 2200);
+    }
+
 }
